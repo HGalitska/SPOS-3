116b82a262e12684cdaf5fec761f9a2733d45b9f
HGalitska <h.galitskaya@gmail.com>
2017-11-14 10:38:09 +0200

    Fixed bug: lottery should run in loop.

--- src/SchedulingAlgorithm.java
+++ src/SchedulingAlgorithm.java
@@ -8,104 +8,113 @@ import java.io.*;
 class SchedulingAlgorithm {
 
     static Results run(int quantum, int runtime, Vector<Process> processVector, Results result, int numberOfTickets, String resultsFile) {
+        result.schedulingType = "Interactive (Preemptive)";
+        result.schedulingName = "Lottery";
+
         int comptime = 0;
         int currentProcess;
         int previousProcess;
         int size = processVector.size();
-        int completed = 0;
+        int completed = 0; // number of processes that finished with their work
 
-        result.schedulingType = "Interactive (Preemptive)";
-        result.schedulingName = "Lottery";
+        try {
 
-       try {
+            PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
 
-           PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
+            Lottery lottery = new Lottery(numberOfTickets);
+            lottery.run(processVector);
+            currentProcess = lottery.getWinner();
 
-           Lottery lottery = new Lottery(numberOfTickets);
-           lottery.run(processVector);
-           currentProcess = lottery.getWinner();
+            Process process = processVector.elementAt(currentProcess);
+            process.arrivaltime = comptime;
+            out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+            
+            while (comptime < runtime) {
 
-           Process process = processVector.elementAt(currentProcess);
-           process.arrivaltime = comptime;
-           out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+                if (process.cpudone == process.cputime) {
 
-          while (comptime < runtime) {
+                    completed++;
+                    out.println(comptime + ":     process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
 
-              // if current process is done
-              if (process.cpudone == process.cputime) {
-                  completed++;
-                  out.println(comptime + ":     process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+                      // if all processes are done
+                    if (completed == size) {
+                        result.compuTime = comptime;
+                        out.close();
+                        return result;
+                    }
 
-                  // if all processes are done
-                  if (completed == size) {
-                      result.compuTime = comptime;
-                      out.close();
-                      return result;
-                  }
+                    int i;
+                    while (true) {
+                        lottery.run(processVector); // run lottery to find out which process should run next
+                        i = lottery.getWinner();
+                        process = processVector.elementAt(i);
+                        if (process.cpudone < process.cputime) {
+                            currentProcess = i;
+                            break;
+                        }
+                    }
 
-                  int i;
-                  while (true) {
-                      lottery.run(processVector); // run lottery to find out which process should run next
-                      i = lottery.getWinner();
-                      process = processVector.elementAt(i);
-                      if (process.cpudone < process.cputime) {
-                          currentProcess = i;
-                          break;
-                      }
-                  }
+                    process = processVector.elementAt(currentProcess);
+                    if (process.cpudone == 0) process.arrivaltime = comptime;
+                    out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+                }
 
-                  process = processVector.elementAt(currentProcess);
-                  if (process.cpudone == 0) process.arrivaltime = comptime;
-                  out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
-              }
-              //BLOCKING ->
+                if (process.ioblocking == process.ionext) {
 
+                    out.println(comptime + ":     process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+                    process.numblocked++;
+                    process.ionext = 0;
+                    previousProcess = currentProcess;
 
-              if (process.ioblocking == process.ionext) {
-                  out.println(comptime + ":     process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
-                  process.numblocked++;
-                  process.ionext = 0;
-                  previousProcess = currentProcess;
+                    int i;
+                    while (true) {
+                        lottery.run(processVector);
+                        i = lottery.getWinner();
+                        process = processVector.elementAt(i);
+                        if (process.cpudone < process.cputime && previousProcess != i) {
+                            currentProcess = i;
+                            break;
+                        }
+                    }
 
-                  lottery.run(processVector);
-                  int i = lottery.getWinner();
-                  process = processVector.elementAt(i);
-                  if (process.cpudone < process.cputime && previousProcess != i) {
-                      currentProcess = i;
-                  }
+                    process = processVector.elementAt(currentProcess);
+                    if (process.cpudone == 0) process.arrivaltime = comptime;
+                    out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+                }
 
-                  process = processVector.elementAt(currentProcess);
-                  if (process.cpudone == 0) process.arrivaltime = comptime;
-                  out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
-              }
+                process.cpudone++;
+                if (process.ioblocking > 0) {
+                    process.ionext++;
+                }
 
-              process.cpudone++;
+                comptime++;
 
-              if (process.ioblocking > 0) {
-                  process.ionext++;
-              }
-              comptime++;
+                // periodic lottery run
+                if (comptime % quantum == 0) {
+                    int i;
+                    while (true) {
+                        lottery.run(processVector);
+                        i = lottery.getWinner();
+                        process = processVector.elementAt(i);
+                        if (process.cpudone < process.cputime) {
+                            currentProcess = i;
+                            break;
+                        }
+                    }
 
-              // periodic lottery run
-              System.out.println(currentProcess);
-              System.out.println(process.cpudone % quantum == 0);
-              if (process.cpudone % quantum == 0) {
-                  lottery.run(processVector);
-                  int i = lottery.getWinner();
-                  process = processVector.elementAt(i);
-                  if (process.cpudone < process.cputime) {
-                      if (process.cpudone == 0) process.arrivaltime = comptime;
-                      currentProcess = i;
-                  }
-                  process = processVector.elementAt(currentProcess);
-                  out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
-              }
-          }
+                    process = processVector.elementAt(currentProcess);
+                    if (process.cpudone == 0) process.arrivaltime = comptime;
+                    out.println(comptime + ":     process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.arrivaltime + ")");
+                }
+                System.out.println(comptime + "<" + runtime);
+            }
 
-          out.close();
-       }  catch (IOException e) { /* Handle exceptions */ }
+           out.close();
 
-       result.compuTime = comptime;
-       return result;
-  }
+        } catch (IOException e) { /* Handle exceptions */ }
+
+        result.compuTime = comptime;
+        return result;
+    }
+
 }
